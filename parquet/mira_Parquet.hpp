134f647b22ae4315cff2cf4260fef233c5a7cf3b
Rin Yokoyama <yokoyama@cns.s.u-tokyo.ac.jp>
2023-12-27 04:38:13 +0900

    add lib for parquet output

---
+++ parquet/mira_Parquet.hpp
@@ -0,0 +1,17 @@
+#ifndef __MIRA_PARQUET_HPP__
+#define __MIRA_PARQUET_HPP__
+#include <iostream>
+#include <arrow/api.h>
+#include <parquet/arrow/writer.h>
+#include <arrow/io/api.h>
+#include <vector>
+#include "mira_decoder.hpp"
+
+namespace mira
+{
+    arrow::MemoryPool *pool = nullptr;
+    std::shared_ptr<arrow::Table> generate_table(const std::vector<mira::EventData> &data);
+    void wirte_parquet_file(std::string file_name, std::shared_ptr<arrow::Table> table);
+}
+
+#endif /*__MIRA_PARQUET_HPP__*/
